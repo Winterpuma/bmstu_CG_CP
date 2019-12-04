fd48c2afb4c65f95d0b284fdbbbe0691a039f238
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-12-04 21:42:22 +0300

    Add roofs to buildings

--- CityWeather/CityWeather/Scene.cs
+++ CityWeather/CityWeather/Scene.cs
@@ -31,9 +31,9 @@ namespace CityWeather
 
         public void CreateScene()
         {
-            CreateGround(Color.Green, size.Width / 2, 400, 0, 500);
+            CreateGround(Color.Green, size.Width / 2, 400, 0, 500, 5);
             CreateCube(Color.DarkOrange, 300, 100, 0, 150, 300);
-            CreateCube(Color.Red, 750, 150, 100, 100, 100);
+            CreateCube(Color.Red, 750, 150, 100, 100, 100, true);
         }
         
         public Scene GetTurnedScene(double tetax, double tetay, double tetaz)
@@ -49,10 +49,10 @@ namespace CityWeather
         }
 
         #region Создание Параллелепипедов
-        public void CreateCube(Color color, int xCent, int dx, int zCent, int dz, int height)
+        public void CreateCube(Color color, int xCent, int dx, int zCent, int dz, int height, bool roof = false)
         {
             Model m = new Model(color);
-
+            
             //передняя
             m.AddVertex(new Point3D(xCent - dx, ground, zCent + dz)); // левая нижняя
             m.AddVertex(new Point3D(xCent + dx, ground, zCent + dz)); // правая нижняя
@@ -72,6 +72,15 @@ namespace CityWeather
             m.CreatePolygon(1, 5, 6, 2); // правая
             m.CreatePolygon(0, 4, 5, 1); // нижняя
 
+            if (roof)
+            {
+                m.AddVertex(new Point3D(xCent, ground - height - 40, zCent));
+                m.CreatePolygon(3, 2, 8);
+                m.CreatePolygon(3, 7, 8);
+                m.CreatePolygon(6, 7, 8);
+                m.CreatePolygon(3, 7, 8);
+            }
+
             scene.Add(m);
         }
 
@@ -85,6 +94,32 @@ namespace CityWeather
             m.AddVertex(new Point3D(xCent + dx, ground, zCent - dz));
 
             m.CreatePolygon(0, 3, 2, 1);
+
+            scene.Add(m);
+        }
+
+        private void CreateGround(Color color, int xCent, int dx, int zCent, int dz, int height)
+        {
+            Model m = new Model(color);
+
+            //передняя
+            m.AddVertex(new Point3D(xCent - dx, ground + height, zCent + dz)); // левая нижняя
+            m.AddVertex(new Point3D(xCent + dx, ground + height, zCent + dz)); // правая нижняя
+            m.AddVertex(new Point3D(xCent + dx, ground, zCent + dz)); // правая верхняя
+            m.AddVertex(new Point3D(xCent - dx, ground, zCent + dz)); // левая верхняя
+
+            // задняя
+            m.AddVertex(new Point3D(xCent - dx, ground + height, zCent - dz)); // левая нижняя
+            m.AddVertex(new Point3D(xCent + dx, ground + height, zCent - dz)); // правая нижняя
+            m.AddVertex(new Point3D(xCent + dx, ground, zCent - dz)); // правая верхняя
+            m.AddVertex(new Point3D(xCent - dx, ground, zCent - dz)); // левая верхняя
+
+            m.CreatePolygon(3, 2, 6, 7); // верхняя
+            m.CreatePolygon(0, 1, 2, 3); // передняя
+            m.CreatePolygon(0, 3, 7, 4); // левая
+            m.CreatePolygon(4, 7, 6, 5); // задняя
+            m.CreatePolygon(1, 5, 6, 2); // правая
+            m.CreatePolygon(0, 4, 5, 1); // нижняя
 
             scene.Add(m);
         }
